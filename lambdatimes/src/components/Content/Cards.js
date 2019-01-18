fc5fa4539083060b19f85dca179a3663823f4cbe
Zooheck <davidscottflack@gmail.com>
2019-01-18 13:56:34 -0600

    Refactor card components into styled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Card from './Card';
 
+const CardsListContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardsListContainer>
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
           {props.cards.map(card => {
             return <Card card={card} />
           })}
-    </div>
+    </CardsListContainer>
   )
 }
 
