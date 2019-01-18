9d7e99b47b153edc53d1273f09c7c10d422162af
Zooheck <davidscottflack@gmail.com>
2019-01-18 12:58:39 -0600

    (STRETCH) Refactor tabs into styled components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,17 +1,57 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
+
+const TabButton = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  :hover {
+    text-decoration: underline;
+  }
+  ${props => 
+  props.selectedTab === props.tab ?
+    css`
+       background-color: #fff;
+       color: #333;
+       border: 2px solid #333;
+    ` :
+    css`
+      display: flex;
+      justify-content: none;
+      align-items: center;
+      flex-direction: row;
+      color: #fff;
+      background-color: #333;
+      margin: 0 5px;
+      padding: 2px 10px;
+      font-size: 12px;
+      letter-spacing: 2px;
+      cursor: pointer;
+      font-weight: bold;
+      :hover {
+        text-decoration: underline;
+      }
+    `
+}
+`
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
-    <div
-      className={props.selectedTab === props.tab ? 'tab active-tab' : 'tab'}
-      onClick={() => props.selectTabHandler(props.tab)}
-    >
+    <TabButton selectedTab={props.selectedTab} tab={props.tab} onClick={() => props.selectTabHandler(props.tab)}>
       {props.tab.toUpperCase()}
-    </div>
+    </TabButton>
   );
 };
 
